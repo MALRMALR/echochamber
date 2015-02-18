d024d3a6be359c7e61fbf92c6a55944d75dd91b4
MALRMALR <michaelalross@gmail.com>
2015-02-18 15:56:00 -0500

    first commit

--- js/main.js
+++ js/main.js
@@ -1,37 +1,60 @@
 $(document).ready(function(){
-	generateVideoContainers();
-	var echoNestJson = 'http://developer.echonest.com/api/v4/genre/artists?api_key=5QG70O4GQ9F8TA30Q&format=json&results=15&bucket=video&name=jazz'
-	$.ajax({
-		type: 'GET',
-		url: echoNestJson,
-		error: function() {
-			$('#container').html('<p>An Error Has Occurred</p>');
-		},
-		dataType: 'json',
-		success: function(data){
-			callEchoNest(data);
+	generateImageContainers();
+	// search function
+	$("#search_button").on("click", function(){
+		var searchTerm = $("#search_term").val();
+		if ($('#artist_button').is(":checked")){
+			var json = 'http://developer.echonest.com/api/v4/artist/search?api_key=5QG70O4GQ9F8TA30Q&format=json&results=15&bucket=video&name='+searchTerm;
+		} else {
+			var json = 'http://developer.echonest.com/api/v4/genre/artists?api_key=5QG70O4GQ9F8TA30Q&format=json&results=15&bucket=video&name='+searchTerm;
 		}
-	})
+			$.ajax({
+				type: 'GET',
+				url: json,
+				error: function() {
+					$('#container').append('<p id="error_message">That is not available</p>');
+				},
+				dataType: 'json',
+				success: function(data){
+					if ($('#error_message')){
+						$('#error_message').empty();
+					}
+					for (var i = 0; i < 15; i++){
+						$('#video_'+i).each(function(){
+							$(this).empty();
+						})
+					}
+					callEchoNest(data);
+				}
+			}) // end api call
+	}) // end search button on click event fxn
 });
 
-function generateVideoContainers(){
+function generateImageContainers(){
 	var container = $('#container');
-	for (var i = 0; i < 150; i++){
-		var videoDiv = '<div class ="video_container" id="video_'+i+'"></div>';
+	for (var i = 0; i < 15; i++){
+		var videoDiv = '<div class="video_container Collage" id="video_'+i+'"></div>';
 		container.append($(videoDiv));
 	}
 }
 
 function callEchoNest(data){
 	var artists = data.response.artists;
-	console.log(artists);
-	for (var i = 0, j = 0, k = 0; i < 15, j < 15, k < 150; i++, j += 1, k++){
-		var url = data.response.artists[i].video[j].image_url;
-		var videoUrl = data.response.artists[i].video[j].url;
-		if (url !== null){
-			$("#video_"+k).append('<a href="'+videoUrl+'" target="_blank"><img src="'+url+'" /></a>')
-		} else {
-			console.log("sorry");
-		}
-	}
+	for (var i = 0; i < 15; i++){
+		var artistCluster = data.response.artists[i];
+		for (j = 0; j < 15; j++){
+			var artistImages = artistCluster.video[j].image_url;
+			var videoUrl = artistCluster.video[j].url;
+			$("#video_"+i).each(function(){
+				$(this).append('<a class="echo_images" href="'+videoUrl+'" target="_blank"><img src="'+artistImages+'" /></a>')
+			}); // end each loop
+		} // end for j
+	} // end for i
 }
+
+$(window).load(function() {
+	$('.Collage').removeWhitespace().collagePlus({
+		'targetHeight': 10,
+		'effect': "effect-1"
+		});
+})
